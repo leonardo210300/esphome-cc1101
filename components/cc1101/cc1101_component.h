8563772473ce13dc6f6401c4454340fabc2cc3d0
leonardo210300 <142263279+leonardo210300@users.noreply.github.com>
2025-02-20 13:36:02 -0400

    Update cc1101_component.h

--- components/cc1101/cc1101_component.h
+++ components/cc1101/cc1101_component.h
@@ -3,17 +3,21 @@
 
 #include <ELECHOUSE_CC1101_SRC_DRV.h>
 #include "esphome/components/remote_transmitter/remote_transmitter.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/core/component.h"
 
-class CC1101 : public PollingComponent, public Sensor {
+class CC1101 : public esphome::PollingComponent, public esphome::sensor::Sensor {
 public:
-    float _freq;
     CC1101(int SCK, int MISO, int MOSI, int CSN, int GDO0, float bandwidth, float freq, esphome::remote_transmitter::RemoteTransmitterComponent* remote_transmitter);
     void setup() override;
     void update() override;
-    void beginTransmission();
-    void endTransmission();
-    void setBW(float bandwidth);
-    void setFreq(float freq);
+    void set_pin_sck(int SCK);
+    void set_pin_miso(int MISO);
+    void set_pin_mosi(int MOSI);
+    void set_pin_csn(int CSN);
+    void set_pin_gdo0(int GDO0);
+    void set_bandwidth(float bandwidth);
+    void set_frequency(float freq);
 
 private:
     int _SCK;
@@ -22,11 +26,10 @@ private:
     int _CSN;
     int _GDO0;
     float _bandwidth;
+    float _freq;
     esphome::remote_transmitter::RemoteTransmitterComponent* _remote_transmitter;
-    float _moduleNumber;
-    int _last_rssi;
-    bool rssi_on;
 };
 
 #endif
+
 
