932eef1306584fa32f3e592e4bbe84b5019efb8d
leonardo210300 <142263279+leonardo210300@users.noreply.github.com>
2025-02-20 13:40:04 -0400

    Update __init__.py

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,26 +1,27 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_PIN
+from esphome.const import CONF_ID, CONF_NAME
 
 # Define the CC1101 class
-cc1101_ns = cg.esphome_ns.namespace("cc1101")
-CC1101 = cc1101_ns.class_("CC1101", cg.PollingComponent, sensor.Sensor)
+cc1101_ns = cg.esphome_ns.namespace('cc1101')
+CC1101 = cc1101_ns.class_('CC1101', cg.PollingComponent, sensor.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CC1101),
-    cv.Required("pin_sck"): pins.gpio_output_pin_schema,
-    cv.Required("pin_miso"): pins.gpio_output_pin_schema,
-    cv.Required("pin_mosi"): pins.gpio_output_pin_schema,
-    cv.Required("pin_csn"): pins.gpio_output_pin_schema,
-    cv.Required("pin_gdo0"): pins.gpio_output_pin_schema,
-    cv.Required("bandwidth"): cv.positive_float,
-    cv.Required("frequency"): cv.positive_float,
-}).extend(sensor.SENSOR_SCHEMA)
+    cv.Required(CONF_NAME): cv.nameable(sensor.SENSOR_SCHEMA),
+    cv.Required("pin_sck"): cv.int_,
+    cv.Required("pin_miso"): cv.int_,
+    cv.Required("pin_mosi"): cv.int_,
+    cv.Required("pin_csn"): cv.int_,
+    cv.Required("pin_gdo0"): cv.int_,
+    cv.Required("bandwidth"): cv.float_,
+    cv.Required("frequency"): cv.float_,
+})
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    cg.add(var.set_name(config[CONF_NAME]))
     cg.add(var.set_pin_sck(config["pin_sck"]))
     cg.add(var.set_pin_miso(config["pin_miso"]))
     cg.add(var.set_pin_mosi(config["pin_mosi"]))
