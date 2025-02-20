6a76bc1b4ce10f1199b9f79d9d95938c82b104ac
leonardo210300 <142263279+leonardo210300@users.noreply.github.com>
2025-02-20 12:02:19 -0400

    Update __init__.py

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -2,7 +2,7 @@
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, remote_transmitter
+from esphome.components import sensor
 from esphome.const import CONF_ID
 
 # Declare the CC1101 class
@@ -10,10 +10,9 @@ CC1101 = cg.global_ns.class_("CC1101", cg.PollingComponent, sensor.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CC1101),
-}).extend(sensor.SENSOR_SCHEMA).extend(remote_transmitter.REMOTE_TRANSMITTER_SCHEMA)
+}).extend(sensor.SENSOR_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield sensor.register_sensor(var, config)
-    yield remote_transmitter.register_remote_transmitter(var, config)
