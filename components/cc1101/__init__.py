54a816d43e09065940d9b38cd0a892d808ff7244
leonardo210300 <142263279+leonardo210300@users.noreply.github.com>
2025-02-20 12:06:18 -0400

    Update __init__.py

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,11 +1,9 @@
-# __init__.py file for the cc1101 component
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
 
-# Declare the CC1101 class
+# Define the CC1101 class
 CC1101 = cg.global_ns.class_("CC1101", cg.PollingComponent, sensor.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
