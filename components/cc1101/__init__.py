e299667df43a57bff682d229aa4db782ee5fb067
leonardo210300 <142263279+leonardo210300@users.noreply.github.com>
2025-02-20 11:59:16 -0400

    Update __init__.py

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,59 +1,20 @@
 # __init__.py file for the cc1101 component
 
-import esphome
-from esphome import automation, component, sensor, remote_transmitter
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, remote_transmitter
+from esphome.const import CONF_ID
 
-# Define the CC1101 class
-class CC1101(component.PollingComponent, sensor.Sensor):
-    def __init__(self, SCK, MISO, MOSI, CSN, GDO0, bandwidth, freq, remote_transmitter):
-        super().__init__()
-        self._SCK = SCK
-        self._MISO = MISO
-        self._MOSI = MOSI
-        self._CSN = CSN
-        self._GDO0 = GDO0
-        self._bandwidth = bandwidth
-        self._freq = freq
-        self._remote_transmitter = remote_transmitter
-        self._moduleNumber = 0
-        self._last_rssi = 0
-        self.rssi_on = False
+CODEOWNERS = ["@leonardo210300"]
 
-    def setup(self):
-        pinMode(self._GDO0, INPUT)
-        ELECHOUSE_cc1101.addSpiPin(self._SCK, self._MISO, self._MOSI, self._CSN, self._moduleNumber)
-        ELECHOUSE_cc1101.setModul(self._moduleNumber)
-        ELECHOUSE_cc1101.Init()
-        ELECHOUSE_cc1101.setRxBW(self._bandwidth)
-        ELECHOUSE_cc1101.setMHZ(self._freq)
-        ELECHOUSE_cc1101.SetRx()
+CONF_CC1101_ID = "cc1101_id"
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CC1101),
+}).extend(sensor.SENSOR_SCHEMA).extend(remote_transmitter.REMOTE_TRANSMITTER_SCHEMA)
 
-    def update(self):
-        rssi = 0
-        if self.rssi_on:
-            ELECHOUSE_cc1101.setModul(self._moduleNumber)
-            rssi = ELECHOUSE_cc1101.getRssi()
-            if rssi != self._last_rssi:
-                self.publish_state(rssi)
-                self._last_rssi = rssi
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
+    yield remote_transmitter.register_remote_transmitter(var, config)
 
-    def beginTransmission(self):
-        ELECHOUSE_cc1101.setModul(self._moduleNumber)
-        ELECHOUSE_cc1101.SetTx()
-        pinMode(self._GDO0, OUTPUT)
-        self._remote_transmitter.setup()
-
-    def endTransmission(self):
-        digitalWrite(self._GDO0, 0)
-        pinMode(self._GDO0, INPUT)
-        ELECHOUSE_cc1101.setModul(self._moduleNumber)
-        ELECHOUSE_cc1101.SetRx()
-        ELECHOUSE_cc1101.SetRx()  # yes, twice
-
-    def setBW(self, bandwidth):
-        ELECHOUSE_cc1101.setModul(self._moduleNumber)
-        ELECHOUSE_cc1101.setRxBW(bandwidth)
-
-    def setFreq(self, freq):
-        ELECHOUSE_cc1101.setModul(self._moduleNumber)
-        ELECHOUSE_cc1101.setMHZ(freq)
