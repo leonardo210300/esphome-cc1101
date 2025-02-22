e658e202f962be94e2800a28b1dde71f971b7ff1
leonardo210300 <142263279+leonardo210300@users.noreply.github.com>
2025-02-22 15:35:24 -0400

    Update __init__.py

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,7 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_NAME
+from esphome import pins
+from esphome.const import (
+    CONF_ID,
+    CONF_MOSI_PIN,
+    CONF_MISO_PIN,
+    CONF_CLK_PIN,
+    CONF_CS_PIN,
+    CONF_NAME,
+    CONF_FORMAT,
+    CONF_FREQUENCY,
+)
 
 # Define the CC1101 class
 cc1101_ns = cg.esphome_ns.namespace('cc1101')
@@ -15,17 +25,19 @@ CONF_PIN_GDO0 = "pin_gdo0"
 CONF_BANDWIDTH = "bandwidth"
 CONF_FREQUENCY = "frequency"
 
+# Configuration schema for the CC1101 component
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CC1101),
     cv.Required(CONF_NAME): cv.string,
-    cv.Required(CONF_PIN_SCK): cv.int_,
-    cv.Required(CONF_PIN_MISO): cv.int_,
-    cv.Required(CONF_PIN_MOSI): cv.int_,
-    cv.Required(CONF_PIN_CSN): cv.int_,
-    cv.Required(CONF_PIN_GDO0): cv.int_,
+    cv.Required(CONF_PIN_SCK): pins.internal_gpio_output_pin_schema,
+    cv.Required(CONF_PIN_MISO): pins.internal_gpio_input_pin_schema,
+    cv.Required(CONF_PIN_MOSI): pins.internal_gpio_output_pin_schema,
+    cv.Required(CONF_PIN_CSN): pins.internal_gpio_output_pin_schema,
+    cv.Required(CONF_PIN_GDO0): pins.internal_gpio_input_pin_schema,
     cv.Required(CONF_BANDWIDTH): cv.float_,
     cv.Required(CONF_FREQUENCY): cv.float_,
 }).extend(sensor.SENSOR_SCHEMA)
+
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
